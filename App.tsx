c963cfa85d8c0b4da251ea7c0a7345ba3206599f
Mayro <deehmyller11@gmail.com>
2022-03-15 17:11:54 -0300

    chore: update app with SignIn screen

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { ThemeProvider } from 'styled-components'
+import { theme } from './src/global/styles/theme'
+
+import { SignIn } from './src/screens/SignIn'
+
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <SignIn />
+    </ThemeProvider>
+  )
+}
