2e9394ea84f48c3766f7bd2a7f40ef09a54a8b4b
Mayro <deehmyller11@gmail.com>
2022-03-16 09:30:53 -0300

    fix: remove status-bar, add in App.tsx

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,4 +1,3 @@
-import { StatusBar } from 'react-native'
 import React from 'react'
 
 import IllustrationImg from '../../assets/illustration.png'
@@ -9,18 +8,12 @@ import ButtonIcon from '../../components/ButtonIcon'
 export function SignIn() {
   return (
     <S.Container>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
       <S.Hero source={IllustrationImg} resizeMode="stretch" />
 
       <S.Content>
         <S.Title>
-          Organize {`\n`}
-          suas jogatinas {`\n`}
-          facilmente
+          Conecte-se {`\n`} e organize suas {`\n`}
+          jogatinas
         </S.Title>
 
         <S.Subtitle>
