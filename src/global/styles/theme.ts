0cc85cebc37ccc7c7eb6761e93e7e3cbb6442686
Mayro <deehmyller11@gmail.com>
2022-03-16 09:28:23 -0300

    chore: update theme, add new colors and setup fonts

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -1,8 +1,27 @@
 export const theme = {
   colors: {
-    background: '#0D133D',
+    brand900: '#0A1033',
+    brand800: '#0D133D',
+    brand700: '#0E1647',
+    brand600: '#1B2565',
+    brand500: '#1D2766',
+    brand400: '#243189',
+    brand300: '#495BCC',
+
     heading: '#DDE3F0',
+    highlight: '#AAB1CC',
     primary: '#E51c44',
-    line: '#991f36'
+    line: '#991f36',
+    on: '#32BD50',
+
+    overlay: 'rgba(0,0,0,0.7))'
+  },
+
+  fonts: {
+    inter400: 'Inter_400Regular',
+    inter500: 'Inter_500Medium',
+
+    rajdhani500: 'Rajdhani_500Medium',
+    rajdhani700: 'Rajdhani_700Bold'
   }
 }
