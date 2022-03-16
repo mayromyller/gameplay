62c12c8d488fd39152bb693d48394e33e8e0f8e5
Mayro <deehmyller11@gmail.com>
2022-03-16 09:29:27 -0300

    chore: import fonts and expo-loading

--- App.tsx
+++ App.tsx
@@ -1,14 +1,35 @@
 import React from 'react'
+import { StatusBar } from 'react-native'
 
-import { ThemeProvider } from 'styled-components'
-import { theme } from './src/global/styles/theme'
+import { useFonts } from 'expo-font'
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter'
+import {
+  Rajdhani_500Medium,
+  Rajdhani_700Bold
+} from '@expo-google-fonts/rajdhani'
+
+import LoadingApp from 'expo-app-loading'
 
 import { SignIn } from './src/screens/SignIn'
 
 export default function App() {
+  const [fontLoading] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold
+  })
+
+  if (!fontLoading) <LoadingApp />
+
   return (
-    <ThemeProvider theme={theme}>
+    <>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       <SignIn />
-    </ThemeProvider>
+    </>
   )
 }
