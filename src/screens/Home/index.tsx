92fdae19cf95dd8a3f2daf5efef95640506a00f5
Mayro <deehmyller11@gmail.com>
2022-03-17 18:23:50 -0300

    chore: update Home screens with CategorySelect

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,17 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { View } from 'react-native'
+import ButtonAdd from '../../components/ButtonAdd'
+import { CategorySelect } from '../../components/CategorySelect'
 
 import { Profile } from '../../components/Profile'
 
 import { Container, styles } from './styles'
 
 export function Home() {
+  const [category, setCategory] = useState('')
+
+  function handleCategorySelect(categoryId: string) {
+    categoryId === category ? setCategory('') : setCategory(categoryId)
+  }
+
   return (
     <Container>
       <View style={styles.header}>
         <Profile />
+        <ButtonAdd activeOpacity={0.7} />
       </View>
+
+      <CategorySelect
+        selectedCategory={category}
+        setCategory={handleCategorySelect}
+      />
     </Container>
   )
 }
