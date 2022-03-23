af255458f8c0e20a6cb31e88512c47f65ea07ea2
Mayro <deehmyller11@gmail.com>
2022-03-23 09:03:42 -0300

    chore: fix divider component

--- src/components/Divider/style.ts
+++ src/components/Divider/style.ts
@@ -2,9 +2,9 @@ import styled from 'styled-components/native'
 import { theme } from '../../global/styles/theme'
 
 export const ViewDivider = styled.View`
-  width: 80%;
+  width: 78%;
   height: 1px;
   background-color: ${theme.colors.brand400};
-  margin: 21px 0;
+  margin: 2px 0 32px;
   align-self: flex-end;
 `
