1f504083ee2dcabab896fb70d3959979dc44e538
Mayro <deehmyller11@gmail.com>
2022-03-30 11:03:57 -0300

    chore: add AppointmentCreate screen in routes

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -5,6 +5,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { SignIn } from '../screens/SignIn'
 import { Home } from '../screens/Home'
 import { AppointmentDetails } from '../screens/AppointmentDetails'
+import { AppointmentCreate } from '../screens/AppointmentCreate'
 
 const Stack = createNativeStackNavigator()
 
@@ -12,6 +13,7 @@ export type RootStackParam = {
   Home: undefined
   SignIn: undefined
   AppointmentDetails: undefined
+  AppointmentCreate: undefined
 }
 
 export function AuthRoutes() {
@@ -27,6 +29,7 @@ export function AuthRoutes() {
       <Stack.Screen name="SignIn" component={SignIn} />
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="AppointmentDetails" component={AppointmentDetails} />
+      <Stack.Screen name="AppointmentCreate" component={AppointmentCreate} />
     </Stack.Navigator>
   )
 }
