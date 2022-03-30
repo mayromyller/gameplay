bf072e2a031dc4a097de6b1c3fbd0994e5d09c95
Mayro <deehmyller11@gmail.com>
2022-03-30 11:03:13 -0300

    chore: add TextArea component

---
+++ src/components/TextArea/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { TextInputProps } from 'react-native'
+
+import * as S from './style'
+
+export function TextArea({ ...rest }: TextInputProps) {
+  return <S.TextInputContainer {...rest} />
+}
---
+++ src/components/TextArea/style.ts
@@ -0,0 +1,15 @@
+import { TextInput } from 'react-native'
+import styled from 'styled-components/native'
+import { theme } from '../../global/styles/theme'
+
+export const TextInputContainer = styled(TextInput)`
+  width: 100%;
+  height: 95px;
+  background-color: ${theme.colors.brand400};
+  color: ${theme.colors.heading};
+  border-radius: 8px;
+  font-family: ${theme.fonts.inter400};
+  font-size: 13px;
+  margin-right: 4px;
+  padding: 4px 12px;
+`
