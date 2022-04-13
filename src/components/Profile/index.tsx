0fedbacd3cb0f4b8b77235d5c071cae061ee4173
Mayro <deehmyller11@gmail.com>
2022-04-13 15:27:37 -0300

    chore: consuming data of user authenticated

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import { View } from 'react-native'
 import { Avatar } from '../Avatar'
+import { useAuth } from '../../hooks/auth'
 
 import * as S from './styles'
 
 export function Profile() {
+  const { user } = useAuth()
+
   return (
     <S.Container>
-      <Avatar urlImage="https://github.com/mayromyller.png" />
+      <Avatar urlImage={user.avatar} />
 
       <View>
         <S.User>
           <S.lightUserName>Olá,</S.lightUserName>
-          <S.UseName>Mmdev</S.UseName>
+          <S.UseName>{user.firstName}</S.UseName>
         </S.User>
 
         <S.Status>Hoje é dia de vitória</S.Status>
