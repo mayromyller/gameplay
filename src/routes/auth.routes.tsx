5e712f2cbd8e53fcb03b2e8059129319b1c32b5c
Mayro <deehmyller11@gmail.com>
2022-04-13 15:25:10 -0300

    chore: remove SingIn screen of app routes

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-import { SignIn } from '../screens/SignIn'
 import { Home } from '../screens/Home'
 import { AppointmentDetails } from '../screens/AppointmentDetails'
 import { AppointmentCreate } from '../screens/AppointmentCreate'
@@ -26,7 +25,6 @@ export function AuthRoutes() {
         }
       }}
     >
-      <Stack.Screen name="SignIn" component={SignIn} />
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="AppointmentDetails" component={AppointmentDetails} />
       <Stack.Screen name="AppointmentCreate" component={AppointmentCreate} />
