fbc050a456edd516a92d9738d44126ea4f41e5ab
Mayro <deehmyller11@gmail.com>
2022-05-23 14:43:31 -0300

    chore: update screen removing static content and add dinamic content

--- src/screens/Guilds/index.tsx
+++ src/screens/Guilds/index.tsx
@@ -1,50 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { FlatList } from 'react-native'
 import { Divider } from '../../components/Divider'
 import { Guild, GuildProps } from '../../components/Guild'
 
 import * as S from './styles'
+import { Load } from '../../components/Load/index'
+import { api } from '../../services/api'
 
 type GuildsProps = {
   handleGuildSelect: (guild: GuildProps) => void
 }
 
 function Guilds({ handleGuildSelect }: GuildsProps) {
-  const guilds = [
-    {
-      id: '1',
-      name: 'Lendários',
-      icon: null,
-      owner: true
-    },
-    {
-      id: '2',
-      name: 'Lendários',
-      icon: null,
-      owner: true
-    }
-  ]
+  const [guilds, setGuilds] = useState<GuildProps[]>([])
+  const [loading, setLoading] = useState(true)
+
+  async function fetchGuilds() {
+    const response = await api.get('/users/@me/guilds')
+
+    setGuilds(response.data)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    fetchGuilds()
+  }, [])
 
   return (
     <S.ViewContainer>
-      <FlatList
-        style={{
-          width: '100%'
-        }}
-        data={guilds}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <Guild data={item} onPress={() => handleGuildSelect(item)} />
-        )}
-        ItemSeparatorComponent={() => <Divider isCentered />}
-        ListHeaderComponent={() => <Divider isCentered />}
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={{
-          paddingBottom: 40,
-          paddingTop: 40
-        }}
-      />
+      {loading ? (
+        <Load />
+      ) : (
+        <FlatList
+          style={{
+            width: '100%'
+          }}
+          data={guilds}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <Guild data={item} onPress={() => handleGuildSelect(item)} />
+          )}
+          ItemSeparatorComponent={() => <Divider isCentered />}
+          ListHeaderComponent={() => <Divider isCentered />}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{
+            paddingBottom: 40,
+            paddingTop: 40
+          }}
+        />
+      )}
     </S.ViewContainer>
   )
 }
