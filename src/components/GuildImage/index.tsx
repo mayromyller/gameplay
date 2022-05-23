b1275bc4679bf627d57a717cd8ed5c00a51fe6fc
Mayro <deehmyller11@gmail.com>
2022-05-23 14:40:40 -0300

    chore: create GuildImage component

--- src/components/GuildImage/index.tsx
+++ src/components/GuildImage/index.tsx
@@ -1,9 +1,31 @@
 import React from 'react'
-import {} from 'react-native'
-// import { theme } from '../../global/styles/theme'
-import * as S from './style'
 
-export function GuildImage() {
-  const uri = 'https://github.com/mayromyller.png'
-  return <S.Image source={{ uri }} resizeMode="cover" />
+import { discordConfig } from '../../config'
+
+import DiscordSVG from '../../assets/discord.svg'
+
+import * as S from './style'
+import { theme } from '../../global/styles/theme'
+
+type ImageProps = {
+  guildId: string
+  iconId: string | null
+}
+
+export function GuildImage({ guildId, iconId }: ImageProps) {
+  const uri = `${discordConfig.CDN_IMAGE}/icons/${guildId}/${iconId}.png`
+
+  return (
+    <S.Container
+      style={{
+        backgroundColor: guildId ? 'transparent' : theme.colors.discord
+      }}
+    >
+      {iconId ? (
+        <S.Image source={{ uri }} resizeMode="cover" />
+      ) : (
+        <DiscordSVG height={40} width={40} />
+      )}
+    </S.Container>
+  )
 }
--- src/components/GuildImage/style.ts
+++ src/components/GuildImage/style.ts
@@ -1,9 +1,17 @@
-import { StyleSheet } from 'react-native'
 import styled from 'styled-components/native'
+import { theme } from '../../global/styles/theme'
+
+export const Container = styled.View`
+  width: 64px;
+  height: 64px;
+  border-radius: 8px;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+  overflow: hidden;
+`
 
 export const Image = styled.Image`
   width: 64px;
   height: 64px;
-  border-radius: 8px;
-  margin-right: 20px;
 `
