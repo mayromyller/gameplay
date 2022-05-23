0fb7aa3319c74deabb6513e87a0bc30c7394651f
Mayro <deehmyller11@gmail.com>
2022-05-23 14:44:28 -0300

    chore: update Home screen with dinamic content and async storage

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback } from 'react'
 
 import { FlatList, View } from 'react-native'
 import ButtonAdd from '../../components/ButtonAdd'
@@ -6,14 +6,18 @@ import { CategorySelect } from '../../components/CategorySelect'
 
 import { Profile } from '../../components/Profile'
 
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
 import { Container, Content, styles } from './styles'
 import { ListHeader } from '../../components/ListHeader'
-import { Appointment } from '../../components/Appointment'
+import { Appointment, AppointmentProps } from '../../components/Appointment'
 import { Divider } from '../../components/Divider'
 import Gradient from '../../components/Gradient'
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, useFocusEffect } from '@react-navigation/native'
 import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 import { RootStackParam } from '../../routes/auth.routes'
+import { COLLECTION_APPOINTMENTS } from '../../config/database'
+import { Load } from '../../components/Load'
 
 type HomeScreenProps = NativeStackNavigationProp<
   RootStackParam,
@@ -22,44 +26,38 @@ type HomeScreenProps = NativeStackNavigationProp<
 
 export function Home() {
   const [category, setCategory] = useState('')
+  const [loading, setLoading] = useState(true)
+  const [appointments, setAppointments] = useState<AppointmentProps[]>([])
 
   const navigation = useNavigation<HomeScreenProps>()
-
-  const appointments = [
-    {
-      id: '1',
-      guild: {
-        id: '1',
-        name: 'Lendários',
-        icon: null,
-        owner: true
-      },
-      category: '1',
-      date: '22/01 às 20:40h',
-      description: 'É hoje que vamos vencer'
-    },
-    {
-      id: '2',
-      guild: {
-        id: '1',
-        name: 'Lendários',
-        icon: null,
-        owner: true
-      },
-      category: '1',
-      date: '22/01 às 20:40h',
-      description: 'É hoje que vamos vencer'
-    }
-  ]
 
   function handleCategorySelect(categoryId: string) {
     categoryId === category ? setCategory('') : setCategory(categoryId)
   }
 
-  const handleAppointmentDetails = () =>
-    navigation.navigate('AppointmentDetails')
+  const handleAppointmentDetails = (guildSelected: AppointmentProps) =>
+    navigation.navigate('AppointmentDetails', { guildSelected })
 
   const handleAppointmentCreate = () => navigation.navigate('AppointmentCreate')
+
+  const loadAppointments = async () => {
+    const storage = await AsyncStorage.getItem(COLLECTION_APPOINTMENTS)
+    const appointment: AppointmentProps[] = storage ? JSON.parse(storage) : []
+
+    if (category) {
+      setAppointments(appointment.filter((item) => item.category === category))
+    } else {
+      setAppointments(appointment)
+    }
+
+    setLoading(false)
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      loadAppointments()
+    }, [category])
+  )
 
   return (
     <Gradient>
@@ -73,23 +71,35 @@ export function Home() {
         setCategory={handleCategorySelect}
       />
 
-      <Content>
-        <ListHeader title="Partidas Agendadas" subtitle="Total 6" />
-      </Content>
+      {loading ? (
+        <Load />
+      ) : (
+        <>
+          <Content>
+            <ListHeader
+              title="Partidas Agendadas"
+              subtitle={`Total ${appointments.length}`}
+            />
+          </Content>
 
-      <FlatList
-        data={appointments}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <Appointment data={item} onPress={handleAppointmentDetails} />
-        )}
-        ItemSeparatorComponent={() => <Divider />}
-        showsVerticalScrollIndicator={false}
-        style={{ marginTop: 24, paddingHorizontal: 24 }}
-        contentContainerStyle={{
-          paddingBottom: 40
-        }}
-      />
+          <FlatList
+            data={appointments}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <Appointment
+                data={item}
+                onPress={() => handleAppointmentDetails(item)}
+              />
+            )}
+            ItemSeparatorComponent={() => <Divider />}
+            showsVerticalScrollIndicator={false}
+            style={{ marginTop: 24, paddingHorizontal: 24 }}
+            contentContainerStyle={{
+              paddingBottom: 40
+            }}
+          />
+        </>
+      )}
     </Gradient>
   )
 }
